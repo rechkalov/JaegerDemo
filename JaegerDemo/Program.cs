3a51ab9ced4ea6bfa7187fa85632bacaabf59355
a.rechkalov <a.rechkalov@2gis.ru>
2021-05-25 14:04:08 +0700

    initial

---
+++ JaegerDemo/Program.cs
@@ -0,0 +1,36 @@
+﻿using System.Threading;
+using Jaeger;
+using Jaeger.Reporters;
+using Jaeger.Samplers;
+using Jaeger.Senders.Thrift;
+
+namespace JaegerDemo
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            using var tracer = new Tracer.Builder("test")
+                .WithSampler(new ConstSampler(true))
+                .WithReporter(new RemoteReporter.Builder()
+                    .WithSender(new UdpSender("localhost", 0, 0))
+                    .Build())
+                .Build();
+
+            using (tracer.BuildSpan("root").StartActive(true))
+            {
+                Thread.Sleep(500);
+                using (tracer.BuildSpan("child-one").StartActive(true))
+                {
+                    Thread.Sleep(200);
+                }
+                Thread.Sleep(400);
+                using (tracer.BuildSpan("child-two").StartActive(true))
+                {
+                    Thread.Sleep(200);
+                }
+                Thread.Sleep(300);
+            }
+        }
+    }
+}
